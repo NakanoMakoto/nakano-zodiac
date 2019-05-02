40d5230894f0d19e6d7df6e8928ce8ae1cf86297
NakanoMakoto <m.nakano8649@gmail.com>
2019-05-02 14:26:34 +0900

    add script

--- src/js/zodiac.js
+++ src/js/zodiac.js
@@ -12,6 +12,12 @@ document.getElementById('button').onclick = function() {
     const date = new Date(y, m - 1, d);//月は「０」を起点とするので−１をする
     moment('date').daysInMonth(); 
     console.log(date);
+
+    // momentが実装できているかの確認
+    const moment_test = moment();
+    const output = moment_test.format('YYYY年MM月DD日 HH:mm:ss dddd');
+    // 現在時刻が表示される
+    console.log(output);
 }
 
 
